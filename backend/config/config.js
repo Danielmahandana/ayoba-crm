c164a7ba93c42c06c8a4972a238b6a8cf577f25f
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-07-27 17:11:36 +0200

    connected to database

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connDB = async () => {
-    await mongoose.connect('mongodb+srv://thusondou30:ayobaApp@cluster0.pbofsvj.mongodb.net/ayoba-crm').then(() => console.log("DB Connected"));
+    await mongoose.connect('mongodb+srv://thusondou:ayobaApp@cluster0.pbofsvj.mongodb.net/ayoba-crm').then(() => console.log("DB Connected"));
 }
