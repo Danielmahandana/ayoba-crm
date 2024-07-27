179abcf34bdbef25ab4f2f52d0134469ef71aec0
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-07-27 21:38:27 +0200

    login authentication

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -7,13 +7,13 @@ import validator from 'validator';
 const userLogin = async (req, res) => {
     const {email, password} = req.body;
     try {
-        const user = await userModel({email});
+        const user = await userModel.findOne({email});
         // check if user exist
         if(!user){
             return res.json({success: false, message:"Invalid Email"})
         }
 
-        // check password
+        // compare password
         const isMatch = await bcrypt.compare(password,user.password);
 
         if(!isMatch){
@@ -21,8 +21,10 @@ const userLogin = async (req, res) => {
         }
 
         const token = createToken(user._id);
+        res.json({success:true,token});
     } catch (error) {
-        
+        console.log(error);
+        res.json({success:false,message:"Error: Failed to authenticate"});
     }
 }
 
