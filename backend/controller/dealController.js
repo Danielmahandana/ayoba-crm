94fde98eb03079502a5d98361ef03c02dbd6742c
Thuso Ndou <104587370+Thuso-Ndou@users.noreply.github.com>
2024-07-28 03:22:19 +0200

    dealController

--- backend/controller/dealController.js
+++ backend/controller/dealController.js
@@ -1,53 +1,26 @@
 import userModel from '../models/dealModel.js';
-import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
-import validator from 'validator';
 
-// create token
-const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET)
-}
-
-// register
-const userRegister = async (req, res) => {
-    const {name,email,password} = req.body;
+// add deal
+const addDeal = async (req, res) => {
+    const {title,value,customer,lead} = req.body;
     try {
-        // check if useer is registered
-        const exists = await userModel.findOne({email});
-        if(exists){
-            return res.json({success: false, message:"Email Already Exists"});
-        }
-
-        // email validator
-        if(!validator.isEmail(email)){
-            return res.json({success: false, message:"Email Is Invalid"});
-        }
-
-        // check password
-        if(password.length < 8){
-            return res.json({success: false, message:"Please Enter Strong Password"});
-        }
-
-        // hash password
-        const salt = await bcrypt.genSalt(10);
-        const hashPassword = await bcrypt.hash(password, salt);
-
         // create a new user
-        const newUser = new userModel({
-            name: name,
-            email: email,
-            password: hashPassword,
+        const newDeal = new dealModel({
+            title: title,
+            value: value,
+            customer: customer,
+            lead: lead
         })
 
         // save user in the database
-        const user = await newUser.save();
-        const token = createToken(user._id);
+        const deal = await newDeal.save();
+        const token = createToken(deal._id);
         res.json({success:true,token});
         
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:"Error: Failed to create user"});
+        res.json({success:false,message:"Error: Failed to add deal"});
     }
 }
 
-export {userRegister}
+export {addDeal}
